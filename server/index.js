2524a550a72bc6d3292d133076c0a39c3686775a
seonae <sunellie55@gmail.com>
2022-02-10 15:17:41 +0900

    mongoose 설치 및 연결

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://seonae:NewSt@healthapp.osrcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {})
+.then(() => console.log('MongoDB Connected!!'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
