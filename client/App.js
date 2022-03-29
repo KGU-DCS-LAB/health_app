837d3cd8df0a37f59e2f8440d4b55123fc015c53
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-03-29 23:06:33 +0900

    Android SafeAreaView 적용

--- client/App.js
+++ client/App.js
@@ -1,4 +1,5 @@
 import React, {Component, useState} from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './components/Login';
@@ -15,48 +16,58 @@ const Stack = createStackNavigator();
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator  initialRouteName='Home' screenOptions={{ headerShown: false }} >
-        <Stack.Screen 
-          name='Home'
-          component={Home}
-        />
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <NavigationContainer>
+        <Stack.Navigator  initialRouteName='Home' screenOptions={{ headerShown: false }} >
+          <Stack.Screen 
+            name='Home'
+            component={Home}
+          />
 
-        <Stack.Screen
-          name='SignIn'
-          component={SignIn}
-        />
+          <Stack.Screen
+            name='SignIn'
+            component={SignIn}
+          />
 
-        <Stack.Screen
-          name='Login'
-          component={Login}
-        />
+          <Stack.Screen
+            name='Login'
+            component={Login}
+          />
 
-        <Stack.Screen
-          name='FindPW'
-          component={FindPW}
-        />
+          <Stack.Screen
+            name='FindPW'
+            component={FindPW}
+          />
 
-        <Stack.Screen
-          name='Main'
-          component={Main}
-        />
+          <Stack.Screen
+            name='Main'
+            component={Main}
+          />
 
-        <Stack.Screen
-          name='ChangePW'
-          component={ChangePW}
-        />
+          <Stack.Screen
+            name='ChangePW'
+            component={ChangePW}
+          />
 
-        <Stack.Screen
-          name='NewsDetail'
-          component={NewsDetail}
-        />
+          <Stack.Screen
+            name='NewsDetail'
+            component={NewsDetail}
+          />
 
-        <Stack.Screen
-          name='ChatBot'
-          component={ChatBot}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name='ChatBot'
+            component={ChatBot}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+});
