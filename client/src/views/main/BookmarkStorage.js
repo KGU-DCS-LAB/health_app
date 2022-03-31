27f108982350a36e0c08a81c4ac26477bdfd1a0d
seonae <sunellie55@gmail.com>
2022-04-01 00:39:18 +0900

    [UPDATE] 븍마크 메뉴 화면 DB 연동, 북마크 생성시 바로 생성된거 볼 수 있도록 동기화 작업 필요

--- client/src/views/main/BookmarkStorage.js
+++ client/src/views/main/BookmarkStorage.js
@@ -1,11 +1,43 @@
-import React from "react";
-import { View, Button } from 'react-native';
-
+import React, {useState} from "react";
+import { View, Modal, Button, NativeBaseProvider, Text, Link, Box, Spacer,  HStack } from 'native-base';
+import { Alert, FlatList } from 'react-native';
+import axios from 'axios';
+const IP_address = process.env.IP_address
 
 export default function BookmarkStorage() {
+    const [dataArr, setDataArr] = useState('');
+    const callback = (data) => {
+        setDataArr(data);
+    }
+
+    axios.get('http://'+IP_address+':5000/bookmarkRouter/find')
+    .then((response) => {
+        callback(response.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+    let bmSArr = Object.values(dataArr).map(bmS => bmS);
+
     return(
-        <View>
-            
-        </View>
+        <NativeBaseProvider>
+<View >
+      <FlatList data={bmSArr} renderItem={({
+      item
+    }) => <Link href="#">
+      <Box borderBottomWidth="1" _dark={{
+      borderColor: "gray.600"
+    }} borderColor="coolGray.200" py="2" >
+            <HStack space={3} justifyContent="space-between">
+                <Text numberOfLines={1} ellipsizeMode='tail' _dark={{
+            color: "warmGray.50"
+          }} color="coolGray.800" bold >
+                  {item.bookmark_name}
+                </Text>
+              <Spacer />
+            </HStack>
+          </Box></Link>}  />
+    </View>
+        </NativeBaseProvider>
     )
 } 
