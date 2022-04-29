ecb2e74ce6bdbf0163c8192868a760b05c3e87f8
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-04-30 03:58:39 +0900

    엑셀 파서 기능 구현 (현재는 콘솔에만 찍힘)

--- stand-alone/web-manager/src/views/data/ExcelParserView.js
+++ stand-alone/web-manager/src/views/data/ExcelParserView.js
@@ -1,6 +1,32 @@
-const ExcelParserView =()=>{
-    return(
-        <div>dd</div>
+
+const readUploadFile = (e) => {
+    e.preventDefault();
+    const xlsx = require('xlsx');
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = xlsx.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = xlsx.utils.sheet_to_json(worksheet);
+            console.log(json);
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
+    }
+}
+
+const ExcelParserView = () => {
+    return (
+        <form>
+            <label htmlFor="upload">Upload File</label>
+            <input
+                type="file"
+                name="upload"
+                id="upload"
+                onChange={readUploadFile}
+            />
+        </form>
     )
 }
 export default ExcelParserView;
