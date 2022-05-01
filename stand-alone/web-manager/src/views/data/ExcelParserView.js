003b1f0065fbb4b29859b40c835a21f58ecaacff
soyoung125 <sypark041754@gmail.com>
2022-05-02 01:38:56 +0900

    json 내용 정리 - 코드 지저분해서 정리해야 함 - 정리한 json db에 저장하기

--- stand-alone/web-manager/src/views/data/ExcelParserView.js
+++ stand-alone/web-manager/src/views/data/ExcelParserView.js
@@ -1,7 +1,10 @@
 
 const readUploadFile = (e) => {
+    const diseaseName = e.target.value.split("_")[2].split(".")[0];
+    console.log(diseaseName)
     e.preventDefault();
     const xlsx = require('xlsx');
+    const result = {}
     if (e.target.files) {
         const reader = new FileReader();
         reader.onload = (e) => {
@@ -10,6 +13,31 @@ const readUploadFile = (e) => {
             const sheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[sheetName];
             const json = xlsx.utils.sheet_to_json(worksheet);
+
+            result.diseaseName = diseaseName;
+            result.patientCount = [];
+            const months = json[2];
+            const total = json[4];
+            let male = []
+            let female = []
+            for (let i = 2; i < 181; i += 5){
+                const key = '__EMPTY_' + i;
+                for (let k = 5; k < json.length; k++){
+                    const datas = json[k]
+                    if(datas.__EMPTY === '남'){
+                        console.log(datas[key])
+
+                        male.push({age: datas.__EMPTY_1, num: datas[key]})
+                    }
+                    if(datas.__EMPTY === '여'){
+                        female.push({age: datas.__EMPTY_1, num: datas[key]})
+                    }
+                }
+                result.patientCount.push({month: months[key], count: {total: total[key], male: male, female: female}})
+                male = [];
+                female = [];
+            }
+            console.log(result.patientCount);
             console.log(json);
         };
         reader.readAsArrayBuffer(e.target.files[0]);
