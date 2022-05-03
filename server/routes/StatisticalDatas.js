ecffa941f2291408d63addc8617cf935aa2c851b
soyoung125 <sypark041754@gmail.com>
2022-05-03 14:10:44 +0900

    발병률 계산하는 중

--- server/routes/StatisticalDatas.js
+++ server/routes/StatisticalDatas.js
@@ -45,13 +45,71 @@ router.post('/save', function (req, res) {
 });
 
 router.get('/calcRate', function(req, res) {
-    console.log('claculate!');
     StatisticalData.find().then( (datas) => {
-        console.log(datas[0]);
+        datas.forEach((data) => {
+            clacAndSave(data, function(result){
+                console.log('done')
+            })
+        })
     }).catch( (err) => {
         console.log(err);
         next(err)
     });
 });
+
+function clacAndSave(data, callback){
+    const result = {}
+    result.calculate = [];
+    let male = []
+    let female = []
+    const statistics = data.statistics;
+    statistics.forEach((statistic) => {
+        // const month = statistic.month.split(' ')[1];
+        // console.log(month);
+        const count = statistic.count;
+
+        count.male.forEach((m) => {
+            if(m.num === '-'){
+                male.push({age: m.age, rate: 0})
+            }else {
+                const rate = m.num / count.total * 100
+                male.push({age: m.age, rate: rate})
+            }
+        })
+        console.log(male)
+        count.female.forEach((f) => {
+            if(f.num === '-'){
+                female.push({age: f.age, rate: 0})
+            }else {
+                const rate = f.num / count.total * 100
+                female.push({age: f.age, rate: rate})
+            }
+        })
+        console.log(female)
+        result.calculate.push({month: statistic.month, rate: {male: male, female: female}})
+        male = [];
+        female = [];
+    })
+    // console.log(result);
+    avgOfRage(result);
+}
+
+const avgOfRage = (datas) => {
+    const result = {}
+    result.calculate = [];
+    let male = []
+    let female = []
+    const calculate = datas.calculate;
+    for (let i=0; i<12; i++){
+        const month = calculate[i].month.split(" ")[1];
+        male = calculate[i].rate.male;
+        for (let k=12; k<calculate.length; k += 12){
+            for(let j=0; j<male.length; j++){
+                // console.log(calculate[k].rate.male[j])
+                // male[j]['rate'] = male[j].rate + calculate[k].rate.male[j].rate
+            }
+        }
+    }
+}
 
 module.exports = router;
