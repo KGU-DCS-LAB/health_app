ee51a01997bcf79160c207d1be830a36d923e598
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-05-04 16:41:50 +0900

    뉴스 디자인 변경

--- client/src/views/news/ShortNewsCard.js
+++ client/src/views/news/ShortNewsCard.js
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
-import { Avatar, Box, HStack, Spacer, VStack } from "native-base";
+import { Avatar, Box, Heading, HStack, Spacer, VStack } from "native-base";
 import { useEffect, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from 'react-native-vector-icons/AntDesign';
 
 
@@ -60,15 +60,15 @@ const ShortNewsCard = (props) => {
                     <HStack space={3} justifyContent="space-between">
                         <Avatar size="48px" source={{
                             uri: undefined
-                        }} 
+                        }}
                         />
                         <VStack>
                             <Text
-                             numberOfLines={1}
-                             ellipsizeMode='tail' 
-                             _dark={{
-                                color: "warmGray.50"
-                            }} color="coolGray.800" bold >
+                                numberOfLines={1}
+                                ellipsizeMode='tail'
+                                _dark={{
+                                    color: "warmGray.50"
+                                }} color="coolGray.800" bold >
                                 기사제목
                             </Text>
                             <Text fontSize="xs" _dark={{
@@ -85,21 +85,41 @@ const ShortNewsCard = (props) => {
     }
 
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={() => navigation.navigate('NewsView', {
-                    // keyword: keyword
-                })}>
-                    <HStack>
-                        <Text>뉴스 더보기</Text>
-                        <Icon name="doubleright" size={15} color="#4F8EF7" />
-                    </HStack>
-                </TouchableOpacity>
-            </View>
-            <NewsComponent/>
-            <NewsComponent/>
-            <NewsComponent/>
+        <View style={styles.card}>
+            <Box pb="3">
+                <HStack>
+                    <Heading size="md" isTruncated>
+                        건강 뉴스
+                    </Heading>
+                    <Spacer />
+                    <TouchableOpacity onPress={() => navigation.navigate('NewsView', {
+                        // keyword: keyword
+                    })}>
+                        <HStack>
+                            <Text>뉴스 더보기</Text>
+                            <Icon name="doubleright" size={15} color="#4F8EF7" />
+                        </HStack>
+                    </TouchableOpacity>
+                </HStack>
+            </Box>
+            {/* 뉴스를 한 3개만 불러와줬으면 함 */}
+            <NewsComponent />
+            <NewsComponent />
+            <NewsComponent />
         </View>
     )
 }
 export default ShortNewsCard;
+
+const styles = StyleSheet.create({
+
+    card: {
+        backgroundColor: '#DAD9FF',
+        marginVertical: 10,
+        borderRadius: 10,
+        // height: 100,
+        padding: 20,
+        marginHorizontal: 10,
+    },
+
+});
